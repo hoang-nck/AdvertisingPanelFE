1a26536e661bfab5e7cf44df9df5d579518ffaf7
hoang.nguyenv <hoang.nguyenv@HOANGNGUYENVAN>
2020-05-25 15:24:13 +0700

    images

--- src/components/cms/image.js
+++ src/components/cms/image.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from 'react'
-import { Image as Img } from 'react-bootstrap'
+import { Card, CardColumns, Modal } from 'react-bootstrap'
 import _ from 'lodash'
 
 import Button from '../common/button'
@@ -53,6 +53,29 @@ const reducer = async (state, action, props) => {
         ...data,
         ...getButton(action.type)
       }
+    case 'showConfirm':
+      return {
+        ...state,
+        showIdx: action.index
+      }
+    case 'onHide':
+      return {
+        ...state,
+        showIdx: -1
+      }
+    case 'delete':
+      data = {}
+      rs = await fileCtr.destroy(state.images[state.showIdx]._id)()
+
+      if (rs.success) {
+        props.commonAc.addAlert({ type: config.alerts.success, title: 'Hình ản', body: 'Xoá thành công!' })
+        state.images.splice(state.showIdx, 1)
+        data.showIdx = -1
+      } else {
+        props.commonAc.addAlert({ type: config.alerts.danger, title: 'Error!', body: rs.message })
+      }
+
+      return {...state, ...data, ...getButton(action.type)}
     default:
       return state
   }
@@ -61,7 +84,7 @@ const reducer = async (state, action, props) => {
 export default function Image (props) {
   const [state, disPatch] = common.useReducer(reducer, initialState, props)
   const fileEl = useRef(null)
-  const { button, file, images } = state
+  const { button, file, images, showIdx } = state
 
   useEffect(() => { disPatch('getImages') }, [])
 
@@ -72,9 +95,27 @@ export default function Image (props) {
       <Button className='clrGreen' noLoading title='Chọn tệp từ máy của bạn' value='Chọn tệp' icon='fas fa-file-image' onClick={() => document.getElementById('file').click()} />
       <span> {_.get(file, 'file.name', '')} </span>
       <Button className='clrBlue' disable={_.isEmpty(file)} loading={button.uploadFile} title='Tải tệp lên hệ thống' value='Tải lên' icon='fas fa-share-square' onClick={() => disPatch('uploadFile')} />
-      <div>
-        {images.map((item, idx) => (<Img key={idx} src={config.serverUrl + item.path} rounded />))}
-      </div>
+      <CardColumns>
+        {images.map((item, idx) => (
+          <Card key={idx}>
+            <Card.Img variant='top' src={config.serverUrl + item.path} />
+            <Card.Body>
+              <Card.Title>{item.name}</Card.Title>
+            </Card.Body>
+            <Button className='clrRed' noLoading title='Xoá hình ảnh' icon='fas fa-trash-alt' onClick={() => disPatch({type: 'showConfirm', index: idx})} />
+          </Card>
+        ))}
+      </CardColumns>
+      <Modal show={showIdx > -1} onHide={() => disPatch('onHide')} centered>
+        <Modal.Header closeButton >
+          <Modal.Title>Xoá hình ảnh</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{`Bạn có chắc là muốn xoá hình ${_.get(images[showIdx], 'name', '')}`}</Modal.Body>
+        <Modal.Footer>
+          <Button name='cancel' noLoading onClick={() => disPatch('onHide')} icon='fa fa-reply-all' value='Huỷ' />
+          <Button name='delete' className='clrBlue' onClick={() => disPatch('delete')} loading={button.delete} icon='fas fa-trash-alt' value='Xoá' />
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
