882ebfab911b315f93db8a9b1861d1ba024afe71
hoang.nguyenv <hoang.nguyenv@HOANGNGUYENVAN>
2020-06-26 17:58:23 +0700

    fix bug

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -5,7 +5,7 @@ const Footer = props => {
   return (
     <React.Fragment>
       <hr className='clshr' />
-      <Row className='clsFooter'>
+      <Row className='clsFooter container-fluid'>
         <Col md={12} lg={9} className='center'><Image width='40' src='/images/advertising.png' /> Bảng Hiệu Chữ Nỗi @ <a className='clsFace' href='tel:0901345535' >0901 345 535</a></Col>
         <Col md={12} lg={3} className='center'><a className='clsFace' href='https://www.facebook.com/HoangNck/' target='_blank' >Hoàng Nck</a> © 2020</Col>
       </Row>
